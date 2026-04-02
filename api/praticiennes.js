79def38ae24dcc6107511fc764aec0c6e2a5d9a4
Grimars <43777446+Grimars@users.noreply.github.com>
2026-04-02 17:00:36 +0200

    Create praticiennes.js

---
+++ api/praticiennes.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  
+  const response = await fetch(
+    `https://api.notion.com/v1/databases/2f75b2304fce800688c3d8820ce9e928/query`,
+    {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${process.env.NOTION_TOKEN}`,
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ page_size: 100 })
+    }
+  );
+  
+  const data = await response.json();
+  res.status(200).json(data);
+}
